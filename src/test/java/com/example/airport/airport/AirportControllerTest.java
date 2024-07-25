af1a243ba4f59357fdf50b35b2104ec7452c40fc
sviridoroman <sviridoroman@gmail.com>
2024-07-25 22:45:59 +0300

    add test for aiport controller

---
+++ src/test/java/com/example/airport/airport/AirportControllerTest.java
@@ -0,0 +1,113 @@
+package com.example.airport.airport;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties.Jwt;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import com.example.airport.flight.Flight;
+import com.example.airport.flight.FlightRepository;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+
+class AirportControllerTest {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @MockBean
+  private AirportService airportService;
+
+  @BeforeEach
+  void setUp() {
+    
+  }
+
+  @Test
+  void testGetAllAirports() throws Exception {
+
+    String airportId = UUID.randomUUID().toString();
+    Airport aiport = Airport.builder()
+    .id(airportId)
+    .name("name")
+    .city("city")
+    .country("country")
+    .flights(new ArrayList<Flight>())
+    .build();
+
+    Mockito.when(airportService.getAllAirports()).thenReturn(Arrays.asList(new SimpleAirportResponce(aiport)));
+  
+    mockMvc
+      .perform(get("http://localhost:8080/airport").with(SecurityMockMvcRequestPostProcessors.user("admin").password("admin").roles("ADMIN"))
+      // .with(SecurityMockMvcRequestPostProcessors.jwt().authorities(new SimpleGrantedAuthority("admin"))))
+      ).andExpect(MockMvcResultMatchers.status().isOk())
+      .andExpect(MockMvcResultMatchers.jsonPath("$[0].name", Matchers.is("name")));
+  }
+
+  @Test
+  void testGetAirportsById() throws Exception {
+    String airportId = UUID.randomUUID().toString();
+    Airport aiport = Airport.builder()
+    .id(airportId)
+    .name("name")
+    .city("city")
+    .country("country")
+    .flights(new ArrayList<Flight>())
+    .build();
+    AirportRequest request = new AirportRequest("name", "city", "country");
+    Mockito.when(airportService.getAirportById(airportId)).thenReturn(new AirportResponce(aiport));
+    ResultActions response = mockMvc.perform(get("http://localhost:8080/airport/{id}",airportId));
+
+    response.andExpect(MockMvcResultMatchers.status().isOk());
+  }
+
+  @Test
+  void testPostAirport() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  void testPutAirport() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  void testDeleteAirport() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  void testAirportController() {
+    fail("Not yet implemented");
+  }
+
+}
